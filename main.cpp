abfe4e1c08a003408bca40fc7cafd0e12a430864
MarcoUnige0226 <91851583+MarcoUnige0226@users.noreply.github.com>
2021-12-01 19:44:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int n;
+    cin>>n;
+    if (numero_primo(2,n)){
+        cout<<"numero primo";
+    }else{
+        cout<<"numero non primo";
+    }
+
+    return 0;
 }
